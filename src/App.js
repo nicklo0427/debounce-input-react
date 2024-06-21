ccfd5521c29dd3beb7645dd6b4ad0bc38025ea45
Nick-Wants-To-Be-A-Billionair <165115252+Nick-Wants-To-Be-A-Billionair@users.noreply.github.com>
2024-06-22 02:40:04 +0800

    fixed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import { useState, useCallback } from "react";
+import { useState, useEffect } from "react";
 // import useDebounce from "./hooks/useDebounce";
 import DebounceInput from "./components/debounceInput";
 export default function App() {
-  const [pokemon, setPokemon] = useState(null);
-
+  const [pokemon, setPokemon] = useState("");
+  const [name, setName] = useState("");
   const handleDataFromChild = (value) => {
-    fetchPokemon(value)
+    // fetchPokemon(value)
+    setName(value);
   }
 
-  
-	const fetchPokemon = useCallback((name) => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-    .then((response) => response.json())
-    .then((data) => setPokemon(data))
-    .catch((error) => console.error(error));
-	}, []);
+  useEffect(() => {
+    if (!name) setPokemon("");
+    else {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      .then((response) => response.json())
+      .then((data) => setPokemon(data))
+      .catch((error) => console.error(error));
+    }
+  }, [name]);
+	// const fetchPokemon = useCallback((name) => {
+  //   fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+  //   .then((response) => response.json())
+  //   .then((data) => setPokemon(data))
+  //   .catch((error) => console.error(error));
+	// }, []);
 
   return <>
-    <DebounceInput onChange={handleDataFromChild} delay={300}
+    <DebounceInput
+      value={name}
+      delay={300}
+      onChange={handleDataFromChild}
      />
     <pre>
       {JSON.stringify(pokemon, null, 2)}
--- src/components/debounceInput.js
+++ src/components/debounceInput.js
@@ -1,17 +1,26 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function DebounceInput({ onChange, value, delay }) {
 	const timeID = useRef(null);
+	const [val, setVal] = useState(value);
 
-	const handleChange = (val) => {
-		clearInterval(timeID.current);
+	useEffect(() => {
 		timeID.current = setTimeout(() => {
 			onChange(val)
 		}, delay);
+		return () => {
+			clearTimeout(timeID.current);
+		};
+	}, [val, onChange, delay]);
+
+	const handleChange = (val) => {
+		setVal(val);
+		
 	};
     return (
         <input
             type="text"
+			value={val}
             onChange={(e) => handleChange(e.target.value)}
             placeholder="請輸入..."
         />
